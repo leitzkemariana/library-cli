c4015ad7629cb5976a683cca3fe5bc06206331de
Mari <leitzkemariana@gmail.com>
2026-01-07 11:27:07 -0300

    Refactor Book class to use private fields and getters/setters

--- library/src/main/java/com/mari/entities/Book.java
+++ library/src/main/java/com/mari/entities/Book.java
@@ -1,12 +1,18 @@
 package com.mari.entities;
 
 public class Book {
-    public Integer id;
-    public String title;
-    public String author;
-    public String publisher;
-    public Integer year;
-    private Client client;
+    private Integer id;
+    private String title;
+    private String author;
+    private String publisher;
+    private Integer year;
+
+    public Book(String title, String author, String publisher, Integer year) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+    }
 
     public Book(Integer id, String title, String author, String publisher, Integer year) {
         this.id = id;
@@ -14,7 +20,42 @@ public class Book {
         this.author = author;
         this.publisher = publisher;
         this.year = year;
-        this.client = null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
     }
 
     public String toString() {
