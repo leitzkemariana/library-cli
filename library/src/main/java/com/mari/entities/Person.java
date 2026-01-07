8f42e2863982c2dbf3358ac69e1fddb94568f242
Mari <leitzkemariana@gmail.com>
2026-01-07 11:31:33 -0300

    Add setters for Person class attributes

--- library/src/main/java/com/mari/entities/Person.java
+++ library/src/main/java/com/mari/entities/Person.java
@@ -11,11 +11,29 @@ public class Person {
         this.password = password;
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public  String getEmail() { return email; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getPassword() { return password; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String toString() {
         return String.format("%nName: %s %nEmail: %s", name, email);
