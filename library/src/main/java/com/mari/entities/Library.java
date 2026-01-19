67c9c7e84da840ae0c9c0f6022a6f5fd86008f06
Mari <leitzkemariana@gmail.com>
2026-01-19 14:21:25 -0300

    Refactor Library class

--- library/src/main/java/com/mari/entities/Library.java
+++ library/src/main/java/com/mari/entities/Library.java
@@ -1,23 +1,23 @@
 package com.mari.entities;
 
-import com.mari.services.HandlerDB;
+import com.mari.services.ConnectionDB;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Library {
-    HandlerDB handlerDB = new HandlerDB();
-    private Connection conn = handlerDB.getConn();
-    private PreparedStatement preparedStatement;
+    private final ConnectionDB connection = new ConnectionDB();
+    private final Connection conn = connection.getConnection();
+    private PreparedStatement statement;
 
     public void addLibrarian(Librarian librarian) {
         try {
-            preparedStatement = conn.prepareStatement("INSERT INTO Librarians (name,email,password) VALUES (?, ?, ?)");
-            preparedStatement.setString(1, librarian.getName());
-            preparedStatement.setString(2, librarian.getEmail());
-            preparedStatement.setString(3, librarian.getPassword());
-            preparedStatement.executeUpdate();
+            statement = conn.prepareStatement("INSERT INTO Librarians (name,email,password) VALUES (?, ?, ?)");
+            statement.setString(1, librarian.getName());
+            statement.setString(2, librarian.getEmail());
+            statement.setString(3, librarian.getPassword());
+            statement.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
