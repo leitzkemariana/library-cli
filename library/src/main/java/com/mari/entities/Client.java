59087821dd12f616b356833cc65ce64e997c77c1
Mari <leitzkemariana@gmail.com>
2026-01-19 14:20:41 -0300

    Refactor Client class

--- library/src/main/java/com/mari/entities/Client.java
+++ library/src/main/java/com/mari/entities/Client.java
@@ -1,6 +1,6 @@
 package com.mari.entities;
 
-import com.mari.services.HandlerDB;
+import com.mari.services.ConnectionDB;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,49 +8,58 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Client extends Person {
-    HandlerDB handlerDB = new HandlerDB();
-    Connection conn = handlerDB.getConn();
+    private final ConnectionDB connection = new ConnectionDB();
+    private final Connection conn = connection.getConnection();
     private PreparedStatement statement;
+    private ResultSet result;
 
     public Client(String name, String email, String password) {
         super(name, email, password);
     }
 
-    public void showBooks (String clientEmail){
-        ResultSet result = null;
+    public PreparedStatement getStatement() {
+        return statement;
+    }
+
+    public ResultSet getResult() {
+        return result;
+    }
+
+    public void showBooks(String clientEmail) {
+        result = null;
 
         try {
             statement = conn.prepareStatement("SELECT * FROM Clients WHERE email = ?");
             statement.setString(1, clientEmail);
             result = statement.executeQuery();
 
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
-        try{
+        try {
             statement = conn.prepareStatement("SELECT * FROM Books WHERE borrowed_by = (?)");
             statement.setInt(1, result.getInt("id"));
             ResultSet resultBook = statement.executeQuery();
 
             System.out.println();
             System.out.println("Borrowed Books: ");
-            while (resultBook.next()){
+            while (resultBook.next()) {
                 System.out.println("Title: " + resultBook.getString("title"));
             }
 
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void searchBook (String bookTitle){
+    public void searchBook(String bookTitle) {
         try {
             statement = conn.prepareStatement("SELECT * FROM Books WHERE title = ?");
             statement.setString(1, bookTitle);
-            ResultSet result = statement.executeQuery();
+            result = statement.executeQuery();
 
-            if (result.next()){
+            if (result.next()) {
                 System.out.println("ID: " + result.getInt("id"));
                 System.out.println("Title: " + result.getString("title"));
                 System.out.println("Author: " + result.getString("author"));
@@ -69,12 +78,13 @@ public class Client extends Person {
                 System.out.println("Book not found");
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void showClient(){
-        super.toString();
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
